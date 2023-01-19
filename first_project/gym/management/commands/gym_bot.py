5a492b33821903df778da45fd34dbfffdb9c2a05
Huntez <osigor5464@gmail.com>
2023-01-19 23:34:45 +0200

    Working with subscription, training and etc...

--- first_project/gym/management/commands/gym_bot.py
+++ first_project/gym/management/commands/gym_bot.py
@@ -7,33 +7,50 @@ from datetime import datetime
 bot = telebot.TeleBot(token=token)
 
 def check(message):
-    return Subscription.objects.filter(
-            user_chat_id__user_chat_id = message.chat.id)
+    if Subscription.objects.get(
+            user_chat_id__user_chat_id = message.chat.id).exercises == 0:
+        return False
+    else:
+        return True
+
+def sub_check(message):
+    if not Subscription.objects.filter(
+            user_chat_id__user_chat_id = message.chat.id):
+        Subscription.objects.get_or_create(
+            user_chat_id = User.objects.get(user_chat_id = message.chat.id),
+            exercises = 0
+        )
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    if not Wallet.objects.filter(user_chat_id = message.chat.id):
-        Wallet.objects.get_or_create(
+    if not User.objects.filter(user_chat_id = message.chat.id):
+        User.objects.get_or_create(
             user_chat_id = message.chat.id
         )
+        Wallet.objects.get_or_create(
+            user_chat_id = User.objects.get(user_chat_id = message.chat.id),
+        )
     
+    sub_check(message)
     if check(message):
         func_list = ['/categories', '/cart', '/payorder',
                      '/checkwallet', '/addtowallet', 
                      '/clearusercart', '/check_workout_exercises',
-                     '/buy_workout_exercises']
+                     '/buy_workout_exercises', '/start_workout',
+                     '/end_workout', '/select_trainer', '/list_workouts']
     else:
         bot.send_message(message.chat.id, 
                 "You dont have workoud exercises! Buy with /buy_workout_exercises")
         func_list = ['/buy_workout_exercises', '/checkwallet',
-                     '/addtowallet']
+                     '/addtowallet', '/list_workouts']
 
     keyboard = telebot.types.ReplyKeyboardMarkup(
         resize_keyboard=True)
 
+    button_list = list()
     for i in func_list:
-        button = telebot.types.KeyboardButton(i)
-        keyboard.add(button)
+        button_list.append(telebot.types.KeyboardButton(i))
+    keyboard.add(*button_list)
 
     bot.send_message(message.chat.id, 
         'Commands : ', reply_markup=keyboard)
@@ -47,11 +64,92 @@ def buy_workout_exercises(message):
             callback_data=obj.name)
         keyboard.add(button)
 
-    bot.send_message(message.chat.id, 'products',
+    bot.send_message(message.chat.id, 'Exercises',
         reply_markup=keyboard)
 
+ 
+@bot.message_handler(commands=['start_workout'], func=check)
+def start_workout(message):
+    time = datetime.now()
+    user = User.objects.get(user_chat_id = message.chat.id)
 
-@bot.message_handler(commands=['check_workout_exercises'])
+    if not user.on_workout:
+        TrainingTime.objects.get_or_create(
+            user_chat_id = User.objects.get(user_chat_id = message.chat.id),
+            start_time = time
+        )
+        
+        user.on_workout = True
+        user.save()
+
+        bot.send_message(message.chat.id, 
+                         f'Workout start time - {time.strftime("%H:%M:%S")}')
+    else:
+        bot.send_message(message.chat.id, 'You already on workout!')
+        
+    bot.send_message(message.chat.id, 'If you want choice avaliable trainer! ' +
+                     '/select_trainer')
+
+@bot.message_handler(commands=['end_workout'], func=check)
+def end_workout(message):
+    time = datetime.now()
+    user = User.objects.get(user_chat_id = message.chat.id)
+    user_train = TrainingTime.objects.filter(
+                user_chat_id__user_chat_id = message.chat.id).latest('user_chat_id')
+    user_sub = Subscription.objects.get(
+            user_chat_id__user_chat_id = message.chat.id)
+
+    if user.on_workout:
+        user.on_workout = False
+        user.save()
+        
+        user_train.end_time = time
+        user_train.save()
+
+        user_sub.exercises -= 1
+        user_sub.save()
+
+        bot.send_message(message.chat.id,
+                         f'End of workout - {time.strftime("%H:%M:%S")}')
+
+        if user_sub.exercises == 0:
+            bot.send_message(message.chat.id, 'Buy exercises for continue!')
+            start(message)
+    else:
+        bot.send_message(message.chat.id, 'You not on workout!')
+
+
+@bot.message_handler(commands=['list_workouts'])
+def list_workouts(message):
+    user_workout_list = list()
+    for i in TrainingTime.objects.filter(
+            user_chat_id__user_chat_id = message.chat.id):
+            user_workout_list.append(
+                f'Start time - {i.start_time},\
+                \nEnd time - {i.end_time}\
+                \nTrainer - {i.trainer}\n')
+
+    bot.send_message(message.chat.id, '\n'.join(user_workout_list))
+
+
+@bot.message_handler(commands=['select_trainer'], func=check)
+def select_trainer(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    trainers = Trainer.objects.filter(free = True)
+
+    if trainers:
+        for obj in trainers:
+            button = telebot.types.InlineKeyboardButton(f'{obj.name} {obj.surname}',
+                callback_data=obj.name)
+            keyboard.add(button)
+
+            bot.send_message(message.chat.id, 'Avaliable trainers :', 
+                             reply_markup=keyboard)
+    else:
+        bot.send_message(message.chat.id, 'Non aviliable trainers!')
+
+
+@bot.message_handler(commands=['check_workout_exercises'], func=check)
 def check_workout_exercises(message):
     count = Subscription.objects.get(
             user_chat_id__user_chat_id = message.chat.id).exercises
@@ -60,8 +158,8 @@ def check_workout_exercises(message):
 
 @bot.message_handler(commands=['checkwallet'])
 def check_wallet(message):
-    bot.send_message(message.chat.id, Wallet.objects.filter(
-    user_chat_id = message.chat.id)[0].balance)
+    bot.send_message(message.chat.id, Wallet.objects.get(
+    user_chat_id__user_chat_id  = message.chat.id).balance)
 
 @bot.message_handler(commands=['addtowallet'])
 def walletsum(message):
@@ -72,8 +170,8 @@ def walletsum(message):
 
 def walletadding(message):
     try:
-        user = Wallet.objects.filter(
-            user_chat_id = message.chat.id)[0]
+        user = Wallet.objects.get(
+            user_chat_id__user_chat_id = message.chat.id)
         user.balance += Decimal(message.text)
         user.save()
     except DecimalException:
@@ -82,8 +180,8 @@ def walletadding(message):
         bot.send_message(message.chat.id, "Summ added to wallet!")
 
 def wallet_pay(message, cost):
-            user = Wallet.objects.filter(
-                    user_chat_id = message.chat.id)[0]
+            user = Wallet.objects.get(
+                    user_chat_id__user_chat_id = message.chat.id)
             if user.balance < cost:
                 bot.send_message(message.chat.id, "Not Enough money!")
                 return False
@@ -91,6 +189,7 @@ def wallet_pay(message, cost):
                 user.balance -= cost
                 user.save()
                 return True
+
 
 @bot.message_handler(commands=['categories'], func=check)
 def categories_button(message):
@@ -164,31 +263,34 @@ def clearusercart(message):
 
 
 @bot.callback_query_handler(func=lambda call: True
+                            if Trainer.objects.filter(name=call.data) else False)
+def start_workout_callback(call):
+    bot.send_message(call.message.chat.id, "KVAAA")
+
+
+@bot.callback_query_handler(func=lambda call: True
                             if Sub_product.objects.filter(name=call.data) else False)
 def sub_buy(call):
     sub_product = Sub_product.objects.get(name = call.data)
 
+    sub_check(call.message)
+
     if wallet_pay(call.message, sub_product.cost):
         bot.send_message(call.message.chat.id, 'Succsess buy!')
 
-        if check(call.message):
-            user = Subscription.objects.get(
-                    user_chat_id__user_chat_id = call.message.chat.id)
-            user.exercises += sub_product.days
-            user.save()
-        else:
-            Subscription.objects.get_or_create(
-                user_chat_id = Wallet.objects.get(user_chat_id = call.message.chat.id),
-                exercises = Sub_product.objects.get(name = call.data).cost
-            )
-            start(call.message)
+        user = Subscription.objects.get(
+                user_chat_id__user_chat_id = call.message.chat.id)
+        user.exercises += sub_product.exercises
+        user.save()
+
+        start(call.message)
 
 
 @bot.callback_query_handler(func=lambda call: True
                             if Product.objects.filter(name=call.data) else False)
 def user_cart(call):
     Users_Cart.objects.get_or_create(
-        user_chat_id = Wallet.objects.get(user_chat_id = call.message.chat.id),
+        user_chat_id = User.objects.get(user_chat_id = call.message.chat.id),
         product = Product.objects.get(name=call.data),
         category = Product.objects.get(name=call.data).category,
         date = datetime.now()
--- first_project/gym/models.py
+++ first_project/gym/models.py
@@ -1,7 +1,15 @@
-import decimal
 from django.db import models
 from django.core.validators import  MinValueValidator 
 from decimal import Decimal
+
+class User(models.Model):
+    user_chat_id = models.PositiveBigIntegerField()
+    on_workout = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user_chat_id)
+
+    # Can be added name, surname, etc..
 
 # Create your models here.
 class Category(models.Model):
@@ -34,7 +42,7 @@ class Product(models.Model):
         return str(self.name)
 
 class Wallet(models.Model):
-    user_chat_id = models.PositiveBigIntegerField()
+    user_chat_id = models.ForeignKey('User', on_delete=models.CASCADE)
     balance = models.DecimalField(
         default=0,
         max_digits=100,
@@ -45,7 +53,7 @@ class Wallet(models.Model):
         return str(self.user_chat_id)
 
 class Users_Cart(models.Model):
-    user_chat_id = models.ForeignKey('Wallet', on_delete=models.CASCADE)
+    user_chat_id = models.ForeignKey('User', on_delete=models.CASCADE)
     product = models.ForeignKey('Product', null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     date = models.DateTimeField()
@@ -54,7 +62,7 @@ class Users_Cart(models.Model):
         return str(self.product)
 
 class Subscription(models.Model):
-    user_chat_id = models.ForeignKey('Wallet', on_delete=models.CASCADE)
+    user_chat_id = models.ForeignKey('User', on_delete=models.CASCADE)
     exercises = models.PositiveIntegerField(default=0) 
 
     def __str__(self):
@@ -62,7 +70,7 @@ class Subscription(models.Model):
 
 class Sub_product(models.Model):
     name = models.CharField(max_length=50)
-    days = models.PositiveIntegerField()
+    exercises = models.PositiveIntegerField()
     cost = models.DecimalField(
         max_digits=10,
         decimal_places=2,
@@ -72,17 +80,26 @@ class Sub_product(models.Model):
     def __str__(self):
         return str(self.name)
 
-class TrainingTime(models.Model):
-    user_chat_id = models.ForeignKey('Subscription', on_delete=models.CASCADE)
-    star_time = models.TimeField()
-    end_time = models.TimeField()
-
 class Trainer(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    start_work = models.TimeField()
-    end_work = models.TimeField()
+    free = models.BooleanField()
+    # start_work = models.TimeField() TODO
+    # end_work = models.TimeField()
 
-class TrainerSignUp(models.Model):
-    user_chat_id = models.ForeignKey('Subscription', on_delete=models.CASCADE)
-    trainer = models.ForeignKey('Trainer', on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.name)
+
+class TrainingTime(models.Model):
+    user_chat_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    trainer = models.ForeignKey(
+            'Trainer', 
+            on_delete=models.CASCADE, 
+            blank=True, 
+            null=True
+    )
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user_chat_id)
